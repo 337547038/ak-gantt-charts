4a6265a5b06c330ef47d7e96f911f3d74731689c
337547038 <337547038@qq.com>
2024-01-19 10:52:00 +0800

    demo

---
+++ docs/assets/test-P87r_keG.js
@@ -0,0 +1,1 @@
+import{_ as e,b as c,e as s}from"./index-KFsMEKdi.js";const t={},o={class:"decision"};function n(r,_){return c(),s("div",o)}const i=e(t,[["render",n]]);export{i as default};
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,23 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'./',
+  base: './',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖2.05/3.02
+      // external: ['vue', 'axios', 'vueRouter']
+      // external: ['tinymce/tinymce']
+      output: {
+        chunkFileNames: info => {
+          //[id].vue这种格式会被编译成_id开头的js，在github里获取不到，这里统一添加js
+          if (info.name.indexOf('_') === 0) {
+            return 'assets/js[name]-[hash].js'
+          } else {
+            return 'assets/[name]-[hash].js'
+          }
+        }
+      }
+    }
   }
 })
